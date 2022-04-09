2168dcd7508615780c4bdd4005d4120836b8253e
gpsp95 <gabrielpspiedade@gmail.com>
2022-04-08 22:12:59 -0300

    Classificador Linear

---
+++ Classificador_Linear.py
@@ -0,0 +1,15 @@
+
+
+class Perceptron:
+    
+    def __init__(self, eta = 0.1, epochs = 50, seed = 1):
+        
+        self._eta = eta
+        self._epochs = epochs
+        self._seed = seed 
+
+
+
+    def fit():
+
+        pass
