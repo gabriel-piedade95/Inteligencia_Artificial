e45d03f1224bf2dce4089d00f2447fd497aa803f
gpsp95 <gabrielpspiedade@gmail.com>
2022-04-09 09:53:35 -0300

    atualização C_L

--- Classificador_Linear.py
+++ Classificador_Linear.py
@@ -11,35 +11,37 @@ class Perceptron:
 
 
 
-def fit(self, X, Y):
-        
+
+
+    def fit(self, X, Y):
+            
         SEED = self._seed
         self.W  = np.array([rand.random() * 10 for x in range(X.shape[1])])
         self.b = np.array([rand.random()*10])
-       
+        
         for epoch in range(self._epochs):
-            
+                
             erros_lista = [0] * Y.shape[0]
             k = 0
-            
+                
             for x_i, y_i in zip(X, Y):
-                
+                    
                 y_hat = int(np.where((sum(self.W*x_i) + self.b) >= 0, 1, -1))
-                
-                
+                    
+                    
                 erro_y = int(y_hat!= y_i)
                 erros_lista[k] = erro_y
-                
-                if erro_y != 0:
                     
+                if erro_y != 0:
+                        
                     D_y = (y_i - y_hat) * self._eta
                     self.W = self.W + (x_i*D_y)
                     self.b = self.b + D_y
+                        
+                    k += 1
                     
-                k += 1
-                
             if sum(erros_lista) == 0:
-                
+                    
                 break
-            
+                
         return self
